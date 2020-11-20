9ade852bac283f32433f4326132cb14da715a0e9
Kasey Jones <joneskm72@gmail.com>
2020-11-19 19:27:22 -0500

    Modified errors

--- client/src/components/TrackCard/index.js
+++ client/src/components/TrackCard/index.js
@@ -7,11 +7,11 @@ const TrackCard = ({ artistname, song }) => {
     <div className="card">
       {/* <img src={image} alt="Avatar" style={{ width: "300px", height: "300px"}}/> */}
       <div className="container">
-        <h4><b> style={{ color: "black" }} {artistname}</b></h4>
-        <h4><b> style={{ color: "black" }} {song}</b></h4>
+        <h4><b style={{ color: "black" }}>{artistname}</b></h4>
+        <h4><b style={{ color: "black" }}>{song}</b></h4>
         <p></p>
       </div>
     </div>
   )};
-
+  
 export default TrackCard;
