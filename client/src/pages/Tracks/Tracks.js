1d3785e5d98d1d5256ac44500cb3bb42c4d1be29
Kasey Jones <joneskm72@gmail.com>
2020-11-19 20:26:26 -0500

    Updated tracks code

--- client/src/pages/Tracks/Tracks.js
+++ client/src/pages/Tracks/Tracks.js
@@ -1,55 +1,22 @@
-import React, { Component } from 'react';
-import TrackCard from "../../components/TrackCard";
-import "./Tracks.css";
+import React, { useContext } from "react";
+import { TracksContext } from "../../utils/TracksContext";
 import { Container } from "../../components/Grid";
-// import Jumbotron from "../../components/Jumbotron";
-import SearchBox from "../../components/SearchBox";
-import Spotify from "../../utils/Spotify";
+import TrackCard from "../../components/TrackCard";
 
-class Tracks extends Component {
-  state = {
-    search: "",
-    tracks: [],
-    results: [],
-    error: ""
-  };
+function Tracks() {
+  const { trackInfoArray, settrackInfoArray }  = useContext(TracksContext);
 
-  componentDidMount() {
-    Spotify.getAccessToken()
-      .then(res => this.setState({ tracks: res.data.message }))
-      .catch(err => console.log(err));
-  }
-
-  handleInputChange = event => {
-    this.setState({ search: event.target.value });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    Spotify.getTrack(this.state.search)
-      .then(res => {
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
-        }
-        this.setState({ results: res.data.message, error: "" });
-      })
-      .catch(err => this.setState({ error: err.message }));
-  };
-  render() {
-    return (
-      <div>
-        <Container style={{ minHeight: "80%" }}>
-          <h1 className="text-center" style={{marginTop: "20px", marginBottom: "20px"}}> Search for a Song </h1>
-          <SearchBox
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
-            tracks={this.state.tracks}
-          />
-          <TrackCard results={this.state.results} />
-        </Container>
-      </div>
-    );
-  }
+  return (
+    <Container fluid>
+      { trackInfoArray && trackInfoArray.map((track, i) => (
+        <TrackCard
+          key={i}
+          image={track.images.length && track.images[0].url}
+          track={track.name}
+        />
+      ))}
+    </Container>
+  );
 }
 
 export default Tracks;
