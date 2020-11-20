ff67e5fbddbd3fd498903ed79673361387c2650f
vkalaparthy <kalaparthy.vani@gmail.com>
2020-11-19 19:37:58 -0500

    changed dashbord to form & added context

---
+++ client/src/pages/Artists/Artists.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+
+import { ArtistsContext } from "../../utils/ArtistsContext";
+import { Container } from "../../components/Grid";
+import ArtistCard from "../../components/ArtistCard";
+
+function Artists() {
+  const { artistInfoArray, setArtistInfoArray }  = useContext(ArtistsContext);
+
+  return (
+    <Container fluid>
+      { artistInfoArray && artistInfoArray.map((artist, i) => (
+        <ArtistCard
+          key={i}
+          image={artist.images.length && artist.images[0].url}
+          artist={artist.name}
+        />
+      ))}
+    </Container>
+  );
+}
+
+export default Artists;
