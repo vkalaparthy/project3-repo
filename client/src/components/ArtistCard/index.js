073635fc732bab9546da5b0bf560ee8e6703f96a
Masiel Bautista <masiel5@users.noreply.github.com>
2020-11-24 09:49:34 -0500

    fixed artists cards for image and text orientation

--- client/src/components/ArtistCard/index.js
+++ client/src/components/ArtistCard/index.js
@@ -25,24 +25,23 @@ const ArtistCard = ({ image, artistname, id, externallink, genre}) => {
   } else {
     return (
       <div className="card mb-3">
-      <div className="row no-gutters">
-        <div className="col-md-4">
-          <img src={image || "https://via.placeholder.com/200x200.png?text=No+Image!"} alt="Avatar" style={{ width: "200px", height: "200px"}} />
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <p className="blackBold">Artist: {artistname}</p>
-            {(genre.length !== 0) && (<ul className="blackBold comma-list">
-              Genres: {genre.slice(0,3).map((item, i) =>
-                <li key={i}>{item}</li>
-              )}
-            </ul>)}
-            <br></br>
-            <p className="link"> <a href={externallink} target="_blank">Check them out on Spotify!<i className="fa fa-spotify"></i></a></p>
-            <button className="btn" value={id} onClick={(event) => getTopSongs(event.target.value)}>Top Songs</button>
+        <div className="row no-gutters">
+          <div className="col-md-4 p-2 justify-content-center">
+            <img src={image || "https://via.placeholder.com/200x200.png?text=No+Image!"} alt="Avatar" style={{ width: "100%", height: "auto"}} />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h1 className="blackBold">Artist: {artistname}</h1>
+                {(genre.length !== 0) && (<ul className="blackBold comma-list">
+                  Genres: {genre.slice(0,3).map((item, i) =>
+                    <li key={i}>{item}</li>
+                  )}
+                </ul>)}
+              <p className="link"> <a href={externallink} target="_blank">Check them out on Spotify!<i className="fa fa-spotify"></i></a></p>
+              <button className="btn" value={id} onClick={(event) => getTopSongs(event.target.value)}>Top Songs</button>
+            </div>
           </div>
         </div>
-      </div>
       </div> 
     )
   }              
