91bec1805285e0966ab40951f24d94b760c2211d
Jarrod Bataille <jtbataille@gmail.com>
2020-11-25 15:51:14 -0500

    making playlist auto-refresh upon add/delete

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,10 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Redirect, Link } from 'react-router-dom';
 import { ArtistsContext } from "../../utils/ArtistsContext";
 import { TracksContext } from "../../utils/TracksContext";
 import { NewReleasesContext } from "../../utils/NewReleasesContext";
 import { PlaylistContext } from "../../utils/PlaylistContext";
 import { Col, Row, Container } from "../../components/Grid";
-import { Input, FormBtn } from '../../components/Form';
 import { Card } from "../../components/Card";
 import Spotify from "../../utils/Spotify";
 
@@ -16,60 +15,36 @@ function Dashboard() {
   const { setArtistInfoArray }  = useContext(ArtistsContext);
   const { setTracksInfoArray } = useContext(TracksContext);
   const { setNewReleasesArray } = useContext(NewReleasesContext);
-  const { playlistArray } = useContext(PlaylistContext);
+  const { playlistArray, setPlaylistArray } = useContext(PlaylistContext);
   
   const [redirectTo, setRedirectTo] = useState(null);
 
   const [searchObject, setSearchObject] = useState({
     type: "",
     query: ""
-  })
-
-  // function handleFormSubmit(event) {
-  //   event.preventDefault();
-  //   console.log("Form submit!!!!");
-  //   console.log(event.target.value);  
-  //   console.log(searchObject);
-  //   Spotify.search(searchObject).then(res => {
-  //     if (searchObject.type === "artist") {
-  //       console.log(res.artists.items);
-  //       setArtistInfoArray(res.artists.items);
-  //       setRedirectTo('/artists');
-  //     } else  {
-  //       // This is for tracks
-  //       setTracksInfoArray(res.tracks.items);
-  //       setRedirectTo('/tracks');
-  //     }
-  //   })
-  //   .catch(err => console.log(err));
-  // };
-
-  // const handleChange = (event) => {
-	// 	setSearchObject({
-  //     ...searchObject,
-	// 		[event.target.name]: event.target.value
-  //   });
-  //   console.log(event.target.name + ": " + event.target.value);
-  // };
+  });
 
   const handleDelete = (id) => {
-    console.log(id);
-    // console.log(event.target);
     API.deleteSong(id);
-    // setRedirectTo("/");
-  } 
+    API.getSongs().then(res => {
+      setPlaylistArray(res.data);
+    });
+  };
+
+  useEffect(() => {
+    API.getSongs().then(res => {
+      setPlaylistArray(res.data);
+    });
+  }, []);
 
   const browseNewReleases = () => {
-    console.log("In browse new releases");
     Spotify.browse({browseType: "newReleases"}).then(res => {
-      console.log(res.albums.items);
       setNewReleasesArray(res.albums.items);
       setRedirectTo('/newreleases');
     })
   };
 
   const handleSearch = () => {
-    console.log("Go to search page");
     setRedirectTo('/search');
   }
 
@@ -117,7 +92,6 @@ function Dashboard() {
                     <a className="pr-1" ><i className="fa fa-play"></i></a> <span></span> 
                     {ele.title} <span></span> 
                     <a className="pr-4" style={{ float: "right"}}><i className="fa fa-trash" onClick={() => handleDelete(ele._id)}></i></a>
-                    {/* <button style={{ float: "right"}} className="btn btn-outline" onClick={() => handleDelete(ele._id)}><i className="fa fa-trash"></i></button> */}
                   </h5> 
                   <h6>
                     <div className="pl-4">{ele.artistname}</div> 
@@ -128,39 +102,6 @@ function Dashboard() {
             </Card>
           </Col>
         </Row>
-
-        {/* <Row>
-          <Col size="md-2"></Col>
-
-          <Col size="md-8">
-            <Card title="Search">
-              <form>
-                <div className="form-group">
-                  <label htmlFor="exampleFormControlSelect1">Select search type</label>
-                  <select name="type"  onChange={handleChange} className="form-control" id="exampleFormControlSelect1">
-                    <option value="" defaultValue>Select an option</option>
-                    <option value="artist">Artist</option>
-                    <option value="track" >Tracks</option>
-                  </select>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="search-option">Enter artist or track</label>
-                  <input 
-                    type="query" 
-                    name="query"
-                    placeholder="Name/Title"
-                    value={searchObject.query}
-                    onChange={handleChange}
-                    className="form-control" id="artistOrTrack"></input>
-                </div>
-                <FormBtn onClick={handleFormSubmit}>Search</FormBtn>
-              </form>
-            </Card>
-          </Col>
-
-          <Col size="md-2"></Col>
-
-        </Row> */}
         
       </Container>
     );      
